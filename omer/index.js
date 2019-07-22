bfd80ce052227e7bb58f4b9c14aa39772302322f
omerH299 <omer299@gmail.com>
2019-07-22 12:34:07 +0300

    Update index.js

--- omer/index.js
+++ omer/index.js
@@ -1,8 +1,9 @@
-$(document).ready(function(){
+function personCtor()
+{
     const name = document.getElementById("name").value
     const age = document.getElementById("age").value
     const password = document.getElementById("password").value
-    var gender = "not selected"
+    let gender = "not selected"
     const genderRadio = document.getElementsByName('gender');
     
     for (let i = 0, length = genderRadio.length; i < length; i++)
@@ -15,11 +16,6 @@ $(document).ready(function(){
     }
     const car = document.getElementById("car").value
     const bike = document.getElementById("bike").checked
-    
-  });
-
-function personCtor()
-{
 
     let person = new PersonFunc(name,age,password,gender,car,bike);
     console.log(person.toString());
@@ -27,6 +23,23 @@ function personCtor()
 
 function personClass()
 {
+    const name = document.getElementById("name").value
+    const age = document.getElementById("age").value
+    const password = document.getElementById("password").value
+    let gender = "not selected"
+    const genderRadio = document.getElementsByName('gender');
+    
+    for (let i = 0, length = genderRadio.length; i < length; i++)
+    {
+        if (genderRadio[i].checked)
+            {
+                gender = genderRadio[i].value
+                break;
+            }
+    }
+    const car = document.getElementById("car").value
+    const bike = document.getElementById("bike").checked
+
     let person = new Person(name,age,password,gender,car,bike);
     console.log(person.toString());
 }
